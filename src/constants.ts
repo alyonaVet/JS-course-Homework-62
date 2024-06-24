b7fa23767705da4a0e794d44a849c95f78e42433
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-24 16:34:10 +0600

    added goodItems;

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,16 @@
 import logoImage from './assets/logo.svg';
+import phoneImg from './assets/img-1.png';
+import laptopImg from './assets/img-2.png';
+import tabletImg from './assets/img-3.png';
+import consoleImg from './assets/img-4.png';
+import headphoneImg from './assets/img-5.png';
 
 export const logoItems = {image: logoImage, text: 'Лучшие цены в интернет-магазинах'};
+
+export const goodItems = [
+  {name: "Смартфоны", image: phoneImg, id: 1},
+  {name: "Ноутбуки", image: laptopImg, id: 2},
+  {name: "Планшеты", image: tabletImg, id: 3},
+  {name: "Игровые приставки", image: consoleImg, id: 4},
+  {name: "Наушники", image: headphoneImg, id: 5},
+];
