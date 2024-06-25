4c4d3ca39bbf264bc71fbfdd4e982055314afe7b
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-25 15:20:24 +0600

    combined all containers in App; added Routes;

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import Home from './containers/Home/Home';
+import {Route, Routes} from 'react-router-dom';
+import Catalog from './containers/Catalog/Catalog';
+import {goodItems, logoItems} from './constants';
+import AboutUs from './containers/AboutUs/AboutUs';
+import Toolbar from './components/Toolbar/Toolbar';
+
 const App = () => {
    return (
        <>
-           Hello, world!
+         <Toolbar image={logoItems.image} text={logoItems.text}/>
+         <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/catalog" element={<Catalog goods={goodItems} />} />
+           <Route path="/aboutus" element={<AboutUs />} />
+           <Route path="*" element={<h3 className="mt-3 mb-5 text-center">Страница не найдена!</h3>} />
+         </Routes>
        </>
+
    );
 };
 
