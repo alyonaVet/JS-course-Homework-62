f256af356c1fa42eb921ee28c946bf1debe76f71
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-25 15:19:37 +0600

    added reviewItems, infoItems, footerItems;

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,10 @@ import laptopImg from './assets/img-2.png';
 import tabletImg from './assets/img-3.png';
 import consoleImg from './assets/img-4.png';
 import headphoneImg from './assets/img-5.png';
+import smartphoneImg from './assets/img-6.png';
+import compImg from './assets/img-7.png';
+import accImg from './assets/img-8.png';
+
 
 export const logoItems = {image: logoImage, text: 'Лучшие цены в интернет-магазинах'};
 
@@ -14,3 +18,18 @@ export const goodItems = [
   {name: "Игровые приставки", image: consoleImg, id: 4},
   {name: "Наушники", image: headphoneImg, id: 5},
 ];
+
+export const reviewItems = [
+  {id: 1, name: 'Обзор смартфонов', image: smartphoneImg},
+  {id: 2, name: 'Обзор компьютеров', image: compImg},
+  {id: 3, name: 'Обзор аксессуаров', image: accImg},
+];
+
+export const infoItems = [
+  {id: 1, title: '1 миллион', text: 'товары доступны в нашем сервисе со всеми необходимыми данными для принятия решения о покупке'},
+  {id: 2, title: '25', text: 'технологические сайты по всему миру, на которых мы просматриваем статьи, дополняющие информацию о продуктах.'},
+  {id: 3, title: '50', text: 'интернет-магазины, где мы отслеживаем цены, чтобы вы могли их сравнивать, а также видеть историю цен и не поддаваться на фальшивые скидки'},
+  {id: 4, title: '6', text: 'раз в сутки мы обновляем данные по всем товарам на нашем сайте и готовы уведомить вас об этих изменениях'},
+];
+
+export const footerItems = {image: logoImage, text: 'Copyright © 2023 Behoof, Inc. Все права защищены.'}
