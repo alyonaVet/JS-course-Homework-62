ebb5aaef99b7a4ef0ca288a7bea853b0372abf1b
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-25 15:18:43 +0600

    refactor component Card;

--- src/components/Goods/Cards/Card.tsx
+++ src/components/Goods/Cards/Card.tsx
@@ -8,11 +8,11 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({image, text}) => {
   return (
-    <div className="card card-hover align-items-center p-2 mb-3" style={{width: '200px'}}>
-      <a className="card-link" href="#">
-        <img src={image} className="card-img-top d-block" style={{width: '120px'}} alt=""/>
-        <div className="card-body d-flex">
-          <p className="card-text mt-auto">{text}</p>
+    <div className="card card-hover align-items-center p-2 mb-3" style={{width:'250px'}}>
+      <a className="card-link d-flex flex-column align-items-center" href="#">
+        <img src={image} className="card-img-top d-block" style={{width:'200px'}} alt={text}/>
+        <div className="card-body">
+          <p className="card-text">{text}</p>
         </div>
       </a>
     </div>
